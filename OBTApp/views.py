dfa8a9d949b8b6dcd58cff2370ae4a2c9dceb46e
ankithpraveen <kith.304@gmail.com>
2020-10-28 16:35:14 +0530

    basic giveform

--- OBTApp/views.py
+++ OBTApp/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
-from .forms import BookForm
+from .forms import *
 # Create your views here.
 
 
 def BookGiveFormView(request):
-    form = BookForm(request.POST or None)
-    if form.is_valid():
-        form.save()
+    book_form = BookForm(request.POST or None)
+    if book_form.is_valid():
+        book_form.save()
+        book_form = BookForm()
+    
+    give_form = GiveForm(request.POST or None)
+    if give_form.is_valid():
+        give_form.save()
+        give_form = GiveForm()
+
+    give_order_form = GiveOrderForm(request.POST or None)
+    if give_order_form.is_valid():
+        give_order_form.save()
+        give_order_form = GiveOrderForm()
+
     context ={
-        'form':form
+        'book_form':book_form,
+        'give_form':give_form,
+        'give_order_form':give_order_form,
     }
     return render(request,"giveform.html",context)
