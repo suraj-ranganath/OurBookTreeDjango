ad458cfc99682cff5af3648807339e8adf243829
suraj-ranganath <suraj.ranganath@gmail.com>
2020-11-10 10:11:16 +0530

    Removed duplicate emailbut form not submitting

--- OBTApp/views.py
+++ OBTApp/views.py
@@ -1,44 +1,46 @@
 from django.shortcuts import render
 from .forms import *
 from .models import *
+from django.db import connection
 # Create your views here.
-
+cursor = connection.cursor()
 
 def BookGiveFormView(request):
     if request.method == 'POST':
         book_form = BookForm(request.POST or None)
         give_order_form = GiveOrderForm(request.POST or None)   
 
-        give_form = GiveForm(request.POST or None,initial={'email':request.user.email})
-
+        #give_form = GiveForm(request.POST or None,initial={'email':request.user.email})
+        '''
         if give_order_form.is_valid() and book_form.is_valid():
             book = book_form.save()
             give_order = give_order_form.save(False)    
 
             give = Give(email=request.user.email)
             give.save()
-            give = give_form.save()
+            #give = give_form.save()
 
             give_order.bookID = book
             give_order.save()
             give_order.giveNo = give 
             book_form = BookForm()
-            give_order_form = GiveOrderForm()   
+            give_order_form = GiveOrderForm()
+        '''   
         context ={
             'book_form':book_form,
             'give_order_form':give_order_form,
-            'give_form':give_form,
+            'email':request.user.email,
         }
         return render(request,"giveform.html",context)
     else:
         book_form = BookForm(request.POST or None)
         give_order_form = GiveOrderForm(request.POST or None) 
-        give_form = GiveForm(request.GET or None, initial = {'email':request.user.email})
+        #give_form = GiveForm(request.GET or None, initial = {'email':request.user.email})
 
         context ={
             'book_form':book_form,
             'give_order_form':give_order_form,
-            'give_form':give_form,
+            #'give_form':give_form,
             'email':request.user.email,
         }
         return render(request,"giveform.html",context)
--- ourBookTree/settings.py
+++ ourBookTree/settings.py
@@ -74,13 +74,17 @@ WSGI_APPLICATION = 'ourBookTree.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'OurBookTree',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
